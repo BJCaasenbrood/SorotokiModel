376edda1d93b2ac95a7130300e2d542a3945a2b4
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-12 14:39:34 +0200

    Minor update

--- src/add/addGravityShapes.m
+++
@@ -1,8 +0,0 @@
-function Shapes = addGravityShapes(Shapes,varargin)   
-
-    if ~isa(varargin{2},'function_handle')
-        func = varargin{2};
-    end
-
-    Shapes.system.Gravity = func;
-end
--- src/backbone.m
+++
@@ -1,6 +0,0 @@
-function [p, ux, uy, uz] = backbone(g)
-    p  = reshape(g(1:3,4,1:end),3,[]).';
-    ux = reshape(g(1:3,1,1:end),3,[]).';
-    uy = reshape(g(1:3,2,1:end),3,[]).';
-    uz = reshape(g(1:3,3,1:end),3,[]).';
-end
