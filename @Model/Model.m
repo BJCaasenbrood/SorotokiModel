218edc16098a0853014a22ce6c3da4954ed5b4e9
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-15 14:22:37 +0200

    Refactor Model class constructor and properties
    - Refactored the Model class constructor to initialize the system property
    - Removed commented out code related to state dimensions, inputs, and other properties
    - Updated the get method to handle multiple input arguments

--- @Model/Model.m
+++ @Model/Model.m
@@ -3,41 +3,12 @@ classdef Model
 properties (Access = public)
     Systems;    
 
+    system;
     options;
     solver;
-    % NDim;         % Total state dimension
-    % NIn;          % Total input dimension
-    % NSys;         % Number of subsystems
-    % NState;       % Number of states per system
-    % NInput;       % Number of inputs per system
-    % TimeStep;     % Solver timesteps
-    % TimeEnd;      % Finite time horizon
-    % Systems;      % Cell of subsystems
-    % Controller;   % Global system controller
-    % Update;       % Update externals (pre-call to controller)
-    % Log;          % Logger
-    
-    % t;            % Time
-    % X;            % States
-    % X0;           % Initial conditions
-    % U;            % Inputs
-    % Y;            % Outputs
-    % K;            % Linear controller
+    NDim;         % Total state dimension
+    NIn;          % Total input dimension
 end
-
-% properties (Access = private)
-%     Flow;         % system flow
-%     Hessian;      % global Hessian matrix
-    
-%     Xtmp;         % Temporary states
-%     Utmp;         % Temporary inputs
-    
-%     MaxIteration;
-%     MaxRebuild;
-%     ResidualNorm;
-    
-%     ShowProcess; 
-% end
 
  %--------------------------------------------------------------------------
 methods  
@@ -46,40 +17,29 @@ function obj = Model(Input,varargin)
     
     obj.options = modeloptions;
     obj.solver  = solveroptions;
-    
-    % obj.NSys = 1;
-    % obj.MaxIteration = 50;
-    % obj.MaxRebuild   = 3;
-    % obj.ResidualNorm = 1e-6;
-    % obj.TimeEnd  = 10;
-    % obj.TimeStep = 1/60;
-    % obj.ShowProcess = true;
-    % obj.Log = struct;
+    obj.system  = struct;
 
-    % obj.NDim = Input.NDim;        
-    % obj.NIn  = Input.NInput;        
-    % obj.Systems{1} = Input;
-    % obj.NState{1}  = Input.NDim;
-    % obj.NInput{1}  = Input.NInput;
+    obj.NDim = 0;
+    obj.NIn  = 0;
+
+    if ~isempty(Input)
+        obj.NDim = Input.getDimension;        
+        obj.NIn  = Input.getNumberInputs;        
+        obj.system.Systems{1} = Input;
+        obj.system.NState{1} = Input.getDimension;
+        obj.system.NInput{1} = Input.getNumberInputs;
+
+        obj.solver.sol.x = Input.solver.sol.x;
+    else
+
+    end
 
     for ii = 1:2:length(varargin)
         obj.(varargin{ii}) = varargin{ii+1};
     end
-    
-    %obj = obj.rebuild();
-    
+
 end
-%---------------------------------------------------------------------- get     
-function varargout = get(Model,varargin)
-    if nargin > 1
-        varargout{nargin-1,1} = [];
-        for ii = 1:length(varargin)
-            varargout{ii,1} = Model.(varargin{ii});
-        end
-    else
-        varargout = Model.(varargin);
-    end
-end       
+    
 
 end
 end
