892b2b84647b59953348109b079db52046a85dd0
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-18 10:22:02 +0200

    Add string function for computing forward kinematics
    This commit adds a new function called "string" to compute the forward kinematics of shapes. The function takes in joint positions and other parameters, and returns the transformation matrix and Jacobian matrix. It also includes some robustness checks and handles near-zero singularities. The implementation uses a Gauss quadrature method for efficiency.

---
+++ @Shapes/string.m
@@ -0,0 +1,27 @@
+function [g, J] = string(Shapes,q,varargin)
+    
+    assert(numel(q) == Shapes.NJoint,['Dimension of joint inconstistent ',...
+            'with POD matrix. Please check your input dimensions dim(q).']);
+    
+    % ensures robustness for near-zero singularities in some PCC models
+    q = q(:) + 1e-12;
+    
+    % if isempty(varargin)
+    % [g, J] = computeForwardKinematicsFast_mex(q,q*0,... % states
+    %     Shapes.beamsolver.SpaceStep,...         % spatial steps
+    %     Shapes.beamsolver.g0(1:3,4),...         % position zero
+    %     Shapes.beamsolver.g0(1:3,1:3),...       % phi zeroclc
+    %     Shapes.beamsolver.Xi0Eval,...    % intrinsic strain vector
+    %     Shapes.beamsolver.ThetaEval,...  % evaluated Theta matrix
+    %     Shapes.beamsolver.DofMap);
+    % else
+    [g, J] = computeForwardKinematicsGaussFast_mex(q,q*0,... % states
+        Shapes.beamsolver.g0(1:3,4),...         % position zero
+        Shapes.beamsolver.g0(1:3,1:3),...       % phi zeroclc
+        Shapes.beamsolver.evalLocal.Xi0,...    % intrinsic strain vector
+        Shapes.beamsolver.evalLocal.Theta,...  % evaluated Theta matrix
+        Shapes.beamsolver.DofMap,...           % dof to strain mapping
+        Shapes.beamsolver.evalLocal.weights,...
+        Shapes.beamsolver.evalLocal.points);  % gauss weights    
+    % end
+end
