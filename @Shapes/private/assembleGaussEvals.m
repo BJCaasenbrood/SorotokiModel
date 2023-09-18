d9c1600a25de98b1efd89c33132f4da4f0d02fb3
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-18 10:31:28 +0200

    Refactor code for assembling Gauss evaluations and global shapes
    - Refactored the code in `assembleGaussEvals.m` to include a new condition for ramp compensation
    - Added logic to calculate `KttEval`, `MttEval`, and `DttEval` based on the ramp compensation condition
    - Updated the code in `assembleGlobalShapes.m` to use the refactored functions for computing forward kinematics and Lagrangian
    - Removed unused variables and commented out code

--- @Shapes/private/assembleGaussEvals.m
+++ @Shapes/private/assembleGaussEvals.m
@@ -14,8 +14,22 @@ for ii = 1:nu
         Shapes.beamsolver.evalLocal.Theta(:,:,ii,jj) = ...
             Shapes.system.pod.Theta(Pl(ii,jj));
         Shapes.beamsolver.evalLocal.Xi0(:,:,ii,jj) = ...
-             Shapes.beamsolver.Xi0(Pl(ii,jj));
+             Shapes.beamsolver.Xi0(Pl(ii,jj));          
     end
+end
+
+for ii = 1:nw
+    if Shapes.options.isRampCompensation
+        TubeRamp = 0.8;
+        alpha = lerp(1,1-TubeRamp,ii/numel(s));
+        Shapes.beamsolver.evalLocal.KttEval(:,:,ii) = (alpha)^2 * Shapes.beamsolver.Ktt;
+        Shapes.beamsolver.evalLocal.MttEval(:,:,ii) = (alpha)^2 * Shapes.beamsolver.Mtt;
+        Shapes.beamsolver.evalLocal.DttEval(:,:,ii) = (alpha)^2 * Shapes.beamsolver.Dtt;
+    else
+        Shapes.beamsolver.evalLocal.KttEval(:,:,ii) = Shapes.beamsolver.Ktt;
+        Shapes.beamsolver.evalLocal.MttEval(:,:,ii) = Shapes.beamsolver.Mtt;
+        Shapes.beamsolver.evalLocal.DttEval(:,:,ii) = Shapes.beamsolver.Dtt;
+    end   
 end
 
 end
--- @Shapes/private/assembleGlobalShapes.m
+++ @Shapes/private/assembleGlobalShapes.m
@@ -8,28 +8,53 @@ if ~isempty(Shapes.options.loadingFactor)
    beta = Shapes.options.loadingFactor;
 end
 
-[g,J,eta] = computeForwardKinematicsFast_mex(Q,dQ,... % states
-    Shapes.beamsolver.SpaceStep,...     % spatial steps
-    Shapes.beamsolver.g0(1:3,4),...     % position zero
-    Shapes.beamsolver.g0(1:3,1:3),...   % phi zero
-    Shapes.beamsolver.Xi0Eval,...       % intrinsic strain vector
-    Shapes.beamsolver.ThetaEval,...     % evaluated Theta matrix
-    Shapes.beamsolver.DofMap);
+[g, J, Jt] = computeForwardKinematicsGaussFast_mex(Q,dQ,...
+    Shapes.beamsolver.g0(1:3,4),...         % position zero
+    Shapes.beamsolver.g0(1:3,1:3),...       % phi zeroclc
+    Shapes.beamsolver.evalLocal.Xi0,...     % intrinsic strain vector
+    Shapes.beamsolver.evalLocal.Theta,...   % evaluated Theta matrix
+    Shapes.beamsolver.DofMap,...            % dof to strain mapping
+    Shapes.beamsolver.evalLocal.weights,...
+    Shapes.beamsolver.evalLocal.points);    % gauss weights    
 
-[M_,C_,K_,R_,D_,fg_,G,...
-    ~,~,~,dJdt_,~,~] = computeLagrangianFast_mex(Q,dQ,...
-    Shapes.beamsolver.SpaceStep,...    % spatial steps
-    Shapes.beamsolver.g0(1:3,4),...    % position zero
-    Shapes.beamsolver.g0(1:3,1:3),...  % phi zeroclc
-    Shapes.beamsolver.Xi0Eval,...      % intrinsic strain vector
-    Shapes.beamsolver.ThetaEval,...    % evaluated Theta matrix
-    Shapes.beamsolver.ActuatorEval,...
-    Shapes.beamsolver.DofMap,...
-    Shapes.beamsolver.KttEval,...
-    Shapes.beamsolver.MttEval,...
+% [g,J,eta] = computeForwardKinematicsFast_mex(Q,dQ,... % states
+%     Shapes.beamsolver.SpaceStep,...     % spatial steps
+%     Shapes.beamsolver.g0(1:3,4),...     % position zero
+%     Shapes.beamsolver.g0(1:3,1:3),...   % phi zero
+%     Shapes.beamsolver.Xi0Eval,...       % intrinsic strain vector
+%     Shapes.beamsolver.ThetaEval,...     % evaluated Theta matrix
+%     Shapes.beamsolver.DofMap);
+
+[M_, C_, K_, fg_] = computeLagrangianGaussFast_mex(Q,dQ,...
+    g,...       % evaluated transformation
+    J,...       % evaluated Jacobian matrix
+    Jt,...      % evalatured time-Jacobian
+    Shapes.beamsolver.evalLocal.Theta,...            % dof to strain mapping
+    Shapes.beamsolver.DofMap,...                     % dof to strain mapping
+    Shapes.beamsolver.evalLocal.MttEval,...
+    Shapes.beamsolver.evalLocal.KttEval,...
     Shapes.beamsolver.DttEval,...
-    Shapes.Material.params.Zeta,...
-    Shapes.system.Gravity);
+    Shapes.system.Gravity,...
+    Shapes.beamsolver.evalLocal.weights,...
+    Shapes.beamsolver.evalLocal.points);    % gauss weights    
+
+R_ = K_ * Shapes.Material.params.Zeta;
+
+% D_ = K_ * 0;
+% [~,~,~,~,D_,~,G,...
+%     ~,~,~,dJdt_,~,~] = computeLagrangianFast_mex(Q,dQ,...
+%     Shapes.beamsolver.SpaceStep,...    % spatial steps
+%     Shapes.beamsolver.g0(1:3,4),...    % position zero
+%     Shapes.beamsolver.g0(1:3,1:3),...  % phi zeroclc
+%     Shapes.beamsolver.Xi0Eval,...      % intrinsic strain vector
+%     Shapes.beamsolver.ThetaEval,...    % evaluated Theta matrix
+%     Shapes.beamsolver.ActuatorEval,...
+%     Shapes.beamsolver.DofMap,...
+%     Shapes.beamsolver.KttEval,...
+%     Shapes.beamsolver.MttEval,...
+%     Shapes.beamsolver.DttEval,...
+%     Shapes.Material.params.Zeta,...
+%     Shapes.system.Gravity);
 
 gg = 0;
 if ( norm(Shapes.system.Gravity) > 0 ) && Shapes.solver.Iteration < 2
@@ -47,19 +72,19 @@ end
     Shapes.system.Mass       = M_;
     Shapes.system.Damping    = R_;
     Shapes.system.Coriolis   = C_;
-    Shapes.system.Viscous    = D_;
+    Shapes.system.Viscous    = K_ * 0;
     Shapes.system.Stiffness  = K_;
     Shapes.system.Tangent    = K_ + beta * gg;
     Shapes.system.Jacobian   = J;
-    Shapes.system.dJacobian  = dJdt_;
+    % Shapes.system.dJacobian  = dJdt_;
     Shapes.system.Backbone   = g;
-    Shapes.system.Velocity   = eta;
+    % Shapes.system.Velocity   = eta;
 
     Shapes.system.fBody      = beta * fg_;
     Shapes.system.fElastic   = K_ * Q;
     Shapes.system.fDamping   = R_ * dQ;
     Shapes.system.fCoriolis  = C_ * dQ;
-    Shapes.system.fDrag      = D_ * dQ;
+    Shapes.system.fDrag      = 0*K_ * dQ;
 
     if isfield(Shapes.system,'Actuator')
         Shapes.system.Input = G;
