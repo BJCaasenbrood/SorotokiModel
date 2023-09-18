5269bcc86b6392a8a8573b411180c2d6c65c6f0f
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-18 10:38:02 +0200

    updated script

--- examples/model/tentacle_arm.m
+++ examples/model/tentacle_arm.m
@@ -1,41 +1,45 @@
 %clr;
-% clr;
+%clr;
+% clf;
 shp = preset.shapes.octarm;
 shp = shp.addGravity();
-
-figure(101);
-view(30,30);
-
-% shp.solver.MaxIteration = 20;
-% shp.solver.TimeStep    = 1/50;
-% shp.solver.TimeHorizon = 5;
-% shp.simulate;
-
-% mdl = Model(shp);
-
-% mdl.solver.MaxIteration = 5;
-% mdl.solver.TimeStep = 1/100;
-% mdl.solver.TimeHorizon = 5;
-
-% mdl = mdl.simulate;
-
-% shp.solver.sol.x = mdl.solver.sol.x(1:5);
-% shp.show();
-% Q = [1;4;0;0;0] * 1e-2;
-
+shp = shp.rebuild;
+% 
+% shp.options.Display = [];
+% 
 shp = shp.simulate();
+% % figure(101);
+% % view(30,30);
 
-% tic;
-% [g] = shp.string(Q);
-% toc;
+% Q = [1;0;1;0;0] * 1e-2;
 
-% shp.system.Mass
-% p = backbone(g);
-% % hold on;
-% fplot(p,'LineW',3);
+
+% tic
+% [g, J] = shp.string(Q);
+% toc
+
+
+% tic
+% [g, J] = shp.string(Q,true);
+% toc
+
+% [p, ux, uy, uz] = backbone(g);
+% fplot(p,'LineW',3); hold on;
+% fquiver(p,ux,0.25,'r','LineW',3);
+% fquiver(p,uy,0.25,'g','LineW',3);
+% fquiver(p,uz,0.25,'b','LineW',3);
+
 % axis equal;
-% view(30,30);
+
+% J(:,:,end)
+
+% % % shp.system.Mass
+% % p = backbone(g);
+% % hold on;
+% % fplot(p,'LineW',3);
+% % axis equal;
+% % view(30,30);
 
 % sum(sqrt(sum(diff(p).^2,2)))
-% axis tight;
+% % axis tight;
 
