772980078cb7ae118ebf7025abf59ceb19cda5a7
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-09-27 14:28:57 +0200

    Rename fluidicsoptions.m, backbone.m, and statespaceoptions.m
    - Renamed systems/fluidics/fluidicsoptions.m to fluidicsoptions.m
    - Renamed backbone.m to show/backbone.m
    - Renamed systems/statespaceoptions.m to statespaceoptions.m

--- fluidicsoptions.m
+++ fluidicsoptions.m
@@ -7,8 +7,6 @@ classdef fluidicsoptions
     
     methods
         function obj = fluidicsoptions
-            %FLUIDICSOTPIONS Construct an instance of this class
-            %   Detailed explanation goes here
             obj.Type         = 'Pressure';
             obj.isRegulated  = true;
         end
