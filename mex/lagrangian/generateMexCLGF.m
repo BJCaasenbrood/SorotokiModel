8254e51343a5c09f48b89c74b809e5c536cca28c
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2024-04-23 11:45:33 +0200

    Update argument types in code generation configuration.
    Adjust argument type for improved functionality.

--- mex/lagrangian/generateMexCLGF.m
+++ mex/lagrangian/generateMexCLGF.m
@@ -7,4 +7,4 @@ cfg.ResponsivenessChecks = false;
 cfg.NumberOfCpuThreads = 16;
 
 codegen -config cfg computeLagrangianGaussFast ...
-    -args {coder.typeof(10,[inf,1]), coder.typeof(10,[inf,1]), coder.typeof(10,[4,4,inf,inf]), coder.typeof(10,[6,inf,inf,inf]), coder.typeof(10,[6,inf,inf,inf]), coder.typeof(10,[inf,inf,inf,inf]), coder.typeof(10,[6,inf]), coder.typeof(10,[6,6,inf,inf]), coder.typeof(10,[6,6,inf,inf]), coder.typeof(10,[6,6,inf,inf]), coder.typeof(10,[3,1]), coder.typeof(10,[inf,inf]), coder.typeof(10,[inf,inf])}
+    -args {coder.typeof(10,[inf,1]), coder.typeof(10,[inf,1]), coder.typeof(10,[4,4,inf,inf]), coder.typeof(10,[6,inf,inf,inf]), coder.typeof(10,[6,inf,inf,inf]), coder.typeof(10,[inf,inf,inf,inf]), coder.typeof(10,[6,inf]), coder.typeof(10,[6,6,inf,inf]), coder.typeof(10,[6,6,inf,inf]), coder.typeof(10,[5,1]), coder.typeof(10,[3,1]), coder.typeof(10,[inf,inf]), coder.typeof(10,[inf,inf])}
